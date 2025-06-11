c23041d5462838e8b04568328422db70f9eec528
Aravind Jaimon <aravindjaimon.dev@gmail.com>
2025-06-11 23:43:33 +0530

    feat(types): added all contentful model types

--- src/lib/contentful/types.ts
+++ src/lib/contentful/types.ts
@@ -1,4 +1,4 @@
-import { EntryFieldTypes } from "contentful";
+import { Entry, EntryFieldTypes } from "contentful";
 
 export type PageEntrySkeleton = {
   contentTypeId: "page";
@@ -7,6 +7,73 @@ export type PageEntrySkeleton = {
     slug: EntryFieldTypes.Symbol;
     metaTitle?: EntryFieldTypes.Symbol;
     metaDescription?: EntryFieldTypes.Symbol;
+    sections?: EntryFieldTypes.Array<
+      EntryFieldTypes.EntryLink<ComponentContentTypes>
+    >;
+  };
+};
+
+export type SlideEntrySkeleton = {
+  contentTypeId: "slide";
+  fields: {
+    title: EntryFieldTypes.Symbol;
+    image: EntryFieldTypes.AssetLink;
+    description?: EntryFieldTypes.Symbol;
+    ctaText?: EntryFieldTypes.Symbol;
+    ctaUrl?: EntryFieldTypes.Symbol;
+  };
+};
+
+export type CarouselEntrySkeleton = {
+  contentTypeId: "carousel";
+  fields: {
+    title: EntryFieldTypes.Symbol;
+    slides: EntryFieldTypes.Array<
+      EntryFieldTypes.EntryLink<SlideEntrySkeleton>
+    >;
+  };
+};
+
+export type TextComponentEntrySkeleton = {
+  contentTypeId: "textComponent";
+  fields: {
+    title?: EntryFieldTypes.Symbol;
+    content: EntryFieldTypes.RichText;
+    alignment?: EntryFieldTypes.Symbol<"left" | "center" | "right">;
+  };
+};
+
+export type ImageComponentEntrySkeleton = {
+  contentTypeId: "imageComponent";
+  fields: {
+    image: EntryFieldTypes.AssetLink;
+    altText: EntryFieldTypes.Symbol;
+    caption?: EntryFieldTypes.Symbol;
+    size?: EntryFieldTypes.Symbol<"small" | "medium" | "large" | "full">;
+  };
+};
+
+export type ImageTextComponentEntrySkeleton = {
+  contentTypeId: "imageTextComponent";
+  fields: {
+    image: EntryFieldTypes.AssetLink;
+    altText: EntryFieldTypes.Symbol;
+    title: EntryFieldTypes.Symbol;
+    content: EntryFieldTypes.RichText;
+    layout: EntryFieldTypes.Symbol<"image-left" | "image-right">;
+  };
+};
+
+export type FooterEntrySkeleton = {
+  contentTypeId: "footer";
+  fields: {
+    companyName: EntryFieldTypes.Symbol;
+    companyDescription?: EntryFieldTypes.Symbol;
+    contactEmail?: EntryFieldTypes.Symbol;
+    contactPhone?: EntryFieldTypes.Symbol;
+    socialLinks?: EntryFieldTypes.Object;
+    footerLinks?: EntryFieldTypes.Object;
+    copyright: EntryFieldTypes.Symbol;
   };
 };
 
@@ -17,3 +84,15 @@ export type NavigationEntrySkeleton = {
     links: EntryFieldTypes.Array<EntryFieldTypes.EntryLink<PageEntrySkeleton>>;
   };
 };
+
+export type ComponentContentTypes =
+  | TextComponentEntrySkeleton
+  | ImageComponentEntrySkeleton
+  | ImageTextComponentEntrySkeleton
+  | CarouselEntrySkeleton;
+
+export type ComponentEntry =
+  | Entry<TextComponentEntrySkeleton, "WITHOUT_UNRESOLVABLE_LINKS", string>
+  | Entry<ImageComponentEntrySkeleton, "WITHOUT_UNRESOLVABLE_LINKS", string>
+  | Entry<ImageTextComponentEntrySkeleton, "WITHOUT_UNRESOLVABLE_LINKS", string>
+  | Entry<CarouselEntrySkeleton, "WITHOUT_UNRESOLVABLE_LINKS", string>;
